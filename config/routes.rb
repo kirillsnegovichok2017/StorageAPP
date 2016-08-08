493fffe72c47361ce792757a89fe5de252baf42f
Alexander-Andrade <veimar.94@gmail.com>
2016-08-08 11:24:41 +0300

    end files

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'home'    =>     'static_pages#home'
   get 'help'    =>     'static_pages#help'
   get 'about'   =>     'static_pages#about'
+  get 'contact' =>     'static_pages#contact'
 
   get 'login'   =>     'sessions#new'
   post 'login'   =>     'sessions#create'
