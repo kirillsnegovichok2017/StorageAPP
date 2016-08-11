bd232efba85fd15a4933d8776abfe0f76d359f88
Alexander-Andrade <veimar.94@gmail.com>
2016-08-11 11:23:01 +0300

    javascript did not works when following the link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
 
+  def full_title(page_title= '')
+    base_title = "Storage"
+    (page_title.empty?) ? base_title : "#{page_title} | #{base_title}"
+  end
 
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,15 @@
 module StaticPagesHelper
+  include ActionView::Helpers::AssetUrlHelper # for asset_path
+  require 'json'
+
+  # def images(dir='')
+  #   (dir.empty?) ? Dir.glob("#{Rails.root.to_s}/app/assets/images/*.{png,jpg}") : Dir.glob("#{Rails.root.to_s}/app/assets/images/"+dir+"/*.{png,jpg}")
+  # end
+
+  def images_path(img_dir='')
+    path = Rails.root.join('app', 'assets', 'images')
+    Dir.chdir(path.to_s)
+    (img_dir.empty?) ? Dir.glob("*.{png,jpg}").map { |img| asset_path img} : Dir.glob( img_dir + "/" +"*.{png,jpg}").map { |img| asset_path img}
+  end
+
 end
