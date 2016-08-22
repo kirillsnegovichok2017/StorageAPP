e1873c15900ec29a527ddc7cfd07add1acc45e6d
Alexander-Andrade <veimar.94@gmail.com>
2016-08-22 14:55:24 +0300

    to forse working mailer push to heroku

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,6 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      debugger
       @user.create_reset_digest
       @user.send_password_reset_email
       flash[:notice] = 'Email sent with password reset instructions'
