f0af973b51311f844407dcce5dae283db74ec058
Alexander-Andrade <veimar.94@gmail.com>
2016-08-23 11:41:41 +0300

    help images has been deleted

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,10 +2,6 @@ module StaticPagesHelper
   include ActionView::Helpers::AssetUrlHelper # for asset_path
   require 'json'
 
-  # def images(dir='')
-  #   (dir.empty?) ? Dir.glob("#{Rails.root.to_s}/app/assets/images/*.{png,jpg}") : Dir.glob("#{Rails.root.to_s}/app/assets/images/"+dir+"/*.{png,jpg}")
-  # end
-
   def images_path(img_dir='')
       path = Rails.root.join('app', 'assets', 'images')
       Dir.chdir(path.to_s)
